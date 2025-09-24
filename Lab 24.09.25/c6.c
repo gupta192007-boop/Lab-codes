c9a13e4f92149a6bf4329840def1ab1308ae936a
gupta192007-boop <gupta192007@gmail.com>
2025-09-24 23:38:03 +0530

    Update c6.c

--- Lab 24.09.25/c6.c
+++ Lab 24.09.25/c6.c
@@ -1,9 +1,10 @@
 //Print no.s from 100 down to 1.
 #include <stdio.h>
 int main(){
-    printf("No.s from 1 to 100 are:\n");
+    printf("No.s from 100 to 1 are:\n");
     for(int i=100;i>=1;i--){
         printf("%d ",i);
     }
     return 0;
+
 }
